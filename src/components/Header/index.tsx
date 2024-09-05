9f9fccfc313cacdeb24bf814e2e2d7207e05196e
MaykonMorais <maykon.morais@protonmail.com>
2024-09-05 10:54:22 -0300

    deploy header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import styles from "./styles";
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <div className="header">
-      <h1 className="lheader__title">{title}</h1>
+      <h1 className="lheader__title">{title} - Teste Header</h1>
 
       <style jsx>{styles}</style>
     </div>
